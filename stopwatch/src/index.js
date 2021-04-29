c642cd171381705eb343db13d3054c71bf7c688a
AlexKrasnyi <https://git.foxminded.com.ua/krasnyi/my-project>
2021-04-29 15:31:09 +0300

    stopwatch v1.1

--- stopwatch/src/index.js
+++ stopwatch/src/index.js
@@ -9,14 +9,11 @@ const waitButton = document.getElementsByClassName('buttons__wait')[0]
 const resetButton = document.getElementsByClassName('buttons__reset')[0]
 
 let data = {h:0, m:0, s:0}
-let time$ = new Observable(observer => {
-    observer.next(data)
+let time$ = new Observable(obs => {
+    obs.next(data)
 })
 time$.subscribe(val => {
-    console.log('val.h', val.h);
-    hours.innerHTML= val.h >= 10 ? val.h : '0' + val.h
-    minutes.innerHTML= val.m >= 10 ? val.m : '0' + val.m
-    seconds.innerHTML = val.s >= 10 ? val.s : '0' + val.s
+    times(data)
 })
 
 
@@ -27,9 +24,6 @@ fromEvent(startButton, 'click')
         stopButton.classList.remove('none')
         int =  interval(1000)
         .subscribe(() => {
-           time$= new Observable (obs => {     
-            obs.next(data)
-           })
            time$.subscribe(val => {
              if (val.s === 59) {
                 val.m++
@@ -59,33 +53,10 @@ fromEvent(stopButton, 'click')
     })
 
 
-let click = 0
-let date1, date2
+
 fromEvent(waitButton, 'click')
 .subscribe(() => {
-  const doubleClic = () => {
-     click++
-     console.log('click', click);
-    if(!date1 && click===1){
-        date1 = new Date().getTime()
-        console.log('date1', date1);
-        return
-    }   
-    if (click >= 2 && !date2) {
-        date2 = new Date().getTime()
-        click = 0
-        console.log('date2', date2);
-    }
-    if (date2 - date1 <= 300) {
-        startButton.classList.remove('none')
-        stopButton.classList.add('none')
-        int.unsubscribe();
-    }
-
-    date1 = 0
-    date2 = 0
-  }
-  doubleClic()
+  doubleClic(int)
 })
 
 
@@ -104,3 +75,24 @@ const times = (data) => {
     minutes.innerHTML= data.m >= 10 ? data.m : '0' + data.m
     seconds.innerHTML = data.s >= 10 ? data.s : '0' + data.s
 }
+
+let click = 0
+let date1, date2
+const doubleClic = (foo) => {
+    click++
+    if(!date1 && click===1){
+        date1 = new Date().getTime()
+        return
+    }   
+    if (click >= 2 && !date2) {
+        date2 = new Date().getTime()
+        click = 0
+    }
+    if (date2 - date1 <= 300) {
+        startButton.classList.remove('none')
+        stopButton.classList.add('none')
+        foo.unsubscribe();
+    }
+    date1 = 0
+    date2 = 0
+  }
